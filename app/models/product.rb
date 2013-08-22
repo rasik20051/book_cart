577f29b4565d380a65e20d3fe99f8aeb9c4a7551
rasik20051 <rasik.wankhede@outlook.com>
2013-08-22 23:12:58 +0530

    CSS issue/CSS update and Product order define

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,18 @@
 class Product < ActiveRecord::Base
 
+	# default_scope order: 'title'
+
 	validates :title,:description,:image_url,:price,presence: true
 	validates :title,uniqueness: true
+	validates :title,length: {minimum: 10}
+
 	validates :price, numericality: {:greater_than_or_equal_to => 0.01}
-	validates :image_url, format: {
-				with: %r{\.(gif|jpg|png)$}i,:multiline => true,
+	validates :image_url, allow_blank: true,format: {
+				with: %r{\.(gif|jpg|png)$}i,multiline: true,
 				message: "Use valid image extension"
 			}
+
+			def self.latest
+				Product.order.(:updated_at).last
+			end
 end
